9af21b22ade32bfe691996a94dd6ac8eee07eb0f
Eroshkin-IBA <eroshkin.artyom.1.21@gmail.com>
2023-04-03 15:55:35 +0300

    Initial commit

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: FlutterMap(
+      options: MapOptions(
+        center: LatLng(0, 0),
+        zoom: 15.3,
+        maxZoom: 17,
+        minZoom: 3.5,
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+          retinaMode: true,
+        ),
+      ],
+    ));
+  }
+}
