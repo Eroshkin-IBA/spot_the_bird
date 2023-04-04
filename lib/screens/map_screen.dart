a5af8069cc485c5bbaacbd2f2a674985eca7ce4d
Eroshkin-IBA <eroshkin.artyom.1.21@gmail.com>
2023-04-04 12:59:49 +0300

    cubit is added

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:spot_the_bird/block/location_cubit.dart';
+
+
 
 class MapScreen extends StatelessWidget {
   @override
